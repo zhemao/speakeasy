627b61bd5f05e8ee1ce9f80f25cdc3af188d2c8f
Zhehao Mao <zhehao.mao@gmail.com>
2012-04-28 01:38:31 -0400

    change settings structure

--- wsgi/settings.py
+++ wsgi/settings.py
@@ -6,7 +6,10 @@ MONGO_USERNAME = os.getenv('OPENSHIFT_NOSQL_DB_USERNAME')
 MONGO_PASSWORD = os.getenv('OPENSHIFT_NOSQL_DB_PASSWORD')
 MONGO_DBNAME = 'speakeasy'
 
-PRIV_KEY = os.getenv('OPENSHIFT_DATA_DIR') + '/server_private.pem'
-PUB_KEY = os.getenv('OPENSHIFT_DATA_DIR') + '/server_public.pem'
+PRIV_KEY_FILE = os.getenv('OPENSHIFT_DATA_DIR') + '/server_private.pem'
+PUB_KEY_FILE = os.getenv('OPENSHIFT_DATA_DIR') + '/server_public.pem'
+
+PRIV_KEY = open(PRIV_KEY_FILE).read()
+PUB_KEY = open(PUB_KEY_FILE).read()
 
 DEBUG = True
