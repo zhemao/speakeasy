97723b82617d6a36ad36dc9ee6c7abe4590eded8
Zhehao Mao <zhehao.mao@gmail.com>
2012-04-28 02:55:26 -0400

    hopefully last syntax error

--- wsgi/api_helpers.py
+++ wsgi/api_helpers.py
@@ -5,7 +5,7 @@ def json_error(result, code):
                 {'Content-Type': 'application/json'})
 
 def json_success():
-    return '{"result":"success"}', 200, {'Content-Type', 'application/json'}
+    return '{"result":"success"}', 200, {'Content-Type': 'application/json'}
 
 def json_result(obj):
     return json.dumps(obj), 200, {'Content-Type': 'application/json'}
