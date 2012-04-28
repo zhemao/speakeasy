724c65fc7bacad39fdb59a58d0a30af1ccc493cf
Zhehao Mao <zhehao.mao@gmail.com>
2012-04-28 00:14:30 -0400

    don't supress rsa key error

--- wsgi/crypto.py
+++ wsgi/crypto.py
@@ -7,8 +7,5 @@ def server_key(app, key='PUB_KEY'):
     return key
 
 def check_signature(pubkey, shibboleth, signature):
-    try:
-        rsakey = RSA.importKey(pubkey)
-        return rsakey.verify(shibboleth, (int(signature),))
-    except ValueError:
-        return False
+    rsakey = RSA.importKey(pubkey)
+    return rsakey.verify(shibboleth, (int(signature),))
