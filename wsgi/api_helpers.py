4044545c4821d164a4a91edf974c76b282799aba
Zhehao Mao <zhehao.mao@gmail.com>
2012-04-28 01:39:39 -0400

    add json helper methods

---
+++ wsgi/api_helpers.py
@@ -0,0 +1,11 @@
+import json
+
+def json_error(result, code):
+    return json.dumps({'result': result}), code, 
+                {'Content-Type', 'application/json'}
+
+def json_success():
+    return '{"result":"success"}', 200, {'Content-Type', 'application/json'}
+
+def json_result(obj):
+    return json.dumps(obj), 200, {'Content-Type', 'application/json'}
--- wsgi/routes.py
+++ wsgi/routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 from flask.ext.pymongo import PyMongo
 from crypto import *
+from api_helpers import *
+from werkzeug import secure_filename
+import gridfs 
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -10,7 +13,7 @@ mongo = PyMongo(app)
 def hello():
     return 'Hello World!'
 
-@app.route('/pubkey', methods=['POST'])
+@app.route('/pubkey/add', methods=['POST'])
 def add_pubkey():
     db = mongo.db
     username = request.form['username']
@@ -20,17 +23,17 @@ def add_pubkey():
 
     key = db.keys.find_one({'username': username})
     if key:
-        return '{"result": "username already exists"}', 400, {}
+        return json_error("username already exists", 400)
 
     if check_signature(pubkey, shibboleth, signature):
         if db.keys.insert({'username': username, 'pubkey': pubkey}):
-            return '{"result": "success"}'
-        return '{"result": "database failure"}', 500, {}
-    return '{"result": "invalid signature"}', 400, {}
+            return json_success()
+        return json_error('database insertion failure', 500)
+    return json_error('invalid signature', 400)
 
 @app.route('/pubkey/server')
 def server_pub():
-    return server_key(app).exportKey(), 200, {'Content-Type': 'text/plain'}
+    return app.config['PUB_KEY'], 200, {'Content-Type': 'text/plain'}
 
 @app.route('/pubkey/<username>')
 def get_pubkey(username):
@@ -42,3 +45,4 @@ def get_pubkey(username):
     else:
         return '', 404, {}
 
+   
